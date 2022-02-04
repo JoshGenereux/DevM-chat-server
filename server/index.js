bb0e0359da1e34e60f4fdd6240527623c282f3a2
JoshGenereux <josh.genereux13@gmail.com>
2022-02-04 08:47:48 -0700

    added controller

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const messages = require('./controllers/messages_controller');
+
+app.use(express.json());
+
+app.
+
+app.listen(3001, ()=>console.log(`Running on server 3001`));
